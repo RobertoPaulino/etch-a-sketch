29054197b8f475731dfae12401c2b6749cc74041
Roberto Paulino <roberto.a.paulino001@gmail.com>
2022-03-11 13:22:46 -0500

    added button to clear grid

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const container = document.querySelector('#container');
 
 
+// create 100 grids, give them 'grid' class, and add an event so that their background changes to black
+// when the mouse hovers over the grid.
 for (i=0; i < 100; i++){
 
 const grid = document.createElement('div');
@@ -12,3 +14,15 @@ grid.addEventListener("mouseenter", function(e) {
 })
 
 }
+
+let gridNode = document.getElementsByClassName('grid');
+
+const clearBtn = document.querySelector('#clear-button')
+
+clearBtn.addEventListener('click', function (e){
+    
+    for( let i = 0; i < gridNode.length; i++){
+        gridNode[i].style.background = "rgb(201, 180, 221)";
+    }
+
+});
