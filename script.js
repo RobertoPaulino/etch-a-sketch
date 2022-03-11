d53512790cdf67fcdc7ae0f3d5146fe247a0eef4
Roberto Paulino <roberto.a.paulino001@gmail.com>
2022-03-11 13:07:48 -0500

    added grid and sketch mouseover logic, added purple background, added header

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-const container = document.querySelector('.container');
+const container = document.querySelector('#container');
 
 
+for (i=0; i < 100; i++){
+
+const grid = document.createElement('div');
+grid.classList.add('grid');
+container.appendChild(grid);
+
+grid.addEventListener("mouseenter", function(e) {
+    e.target.style.background = "black";
+})
+
+}
